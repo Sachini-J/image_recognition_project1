ef7e466d87cd43fa8e61ae3360e40da90f0f6e5a
SachiniJ <sc15535@student.samk.fi>
2026-03-23 22:57:07 +0200

    Update to support training observation: main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from args import get_args
 import pandas as pd
 import os
 from dataset import ObjDetectionDataset
+import torch
 from torch.utils.data import DataLoader
 from model import build_model
+from trainer import train_model
 
 def collate(batch):
     images, targets = zip(*batch)
@@ -11,28 +13,31 @@ def collate(batch):
 
 def main():
     args = get_args()
-
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
     # 1. Read the dataframes
     train_df = pd.read_csv("CSVs/train_df.csv")
     val_df = pd.read_csv("CSVs/val_df.csv")
-    #train_df = pd.read_csv(os.path.join(args.csv_dir,'train_df.csv'))
-    #val_df = pd.read_csv(os.path.join(args.csv_dir,'val_df.csv'))
 
     # 2. Prepare dataset
-    train_dataset = ObjDetectionDataset(train_df)
-    val_dataset = ObjDetectionDataset(val_df)
+    train_dataset = ObjDetectionDataset(train_df, args)
+    val_dataset = ObjDetectionDataset(val_df, args)
+
+    #print(train_df.columns)
 
     # 3. Create data loaders
-    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate)
-    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collate)
-    # Why we shuffle trainset and dont shuffle valset??
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate, num_workers=0, pin_memory=torch.cuda.is_available())
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collate, num_workers=0, pin_memory=torch.cuda.is_available())
 
     #images, targets = next(iter(train_loader))
 
     # 4. Initialization the model
-    model = build_model(args.backbone)
+    model = build_model(args.backbone, num_classes=args.num_classes + 1)
 
-    print("me")
+    # 5. Train the model
+    train_model(model, train_loader, val_loader, device)
+
+    
 
 if __name__ == '__main__':
     main()
