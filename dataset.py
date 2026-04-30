98634a08e401ac273a539e1ee9c74ef1120960fe
SachiniJ <sc15535@student.samk.fi>
2026-04-30 21:26:33 +0300

    Update dataset to use composed augmentations pipeline

--- dataset.py
+++ dataset.py
@@ -3,37 +3,30 @@ from PIL import Image
 from torchvision.transforms.functional import to_tensor
 from args import get_args
 from utils import resize_box_xyxy
-import random
-from torchvision.transforms.functional import hflip
-import aumentation as aug
+import augmentations as aug
 
 class ObjDetectionDataset(torch.utils.data.Dataset):
-    def __init__(self, df, args, transform=None):
+    def __init__(self, df, transform=None):
         self.df = df.reset_index(drop=True)
-        self.args = args
+        self.transform = transform
 
         if transform is None:
-            self.transform = aug.NoTransform()
+            self.transform = [aug.NoTransform()]
         else:
-            self.transform = transform
+            self.transform = aug.Compose(transform)
 
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, idx):
+        args = get_args
         # TODO 1: Get the row number idx from dataframe
         # your code here
         row = self.df.iloc[idx]
 
         img = Image.open(row["Images"]).convert("RGB")
-        img = Ima
-        w, h = img.size
         
-        # Data Aumentation
-        if random.random() > 0.5:
-            img = hflip(img)
-
-        img = img.resize((self.args.image_size, self.args.image_size))
+        w, h = img.size
         
         image = to_tensor(img)
 
@@ -46,8 +39,6 @@ class ObjDetectionDataset(torch.utils.data.Dataset):
                 x2 = (xc + bw/2) * w
                 y2 = (yc + bh/2) * h
                 
-                x1, y1, x2, y2 = resize_box_xyxy((x1, y1, x2, y2),  w, h , self.args.image_size, self.args.image_size)
-                
                 boxes.append([x1, y1, x2, y2])
                 labels.append(int(cls) + 1)
 
@@ -58,6 +49,7 @@ class ObjDetectionDataset(torch.utils.data.Dataset):
         }
         # TODO 2: Return what you need from this class
         # your code here
-        image, target = self.transform(image, target)
-        
+        image,target = self.transform(image,target)
+        image = image.contiguous().clone()  
+      
         return image, target
